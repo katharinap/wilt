b20a059d8210fc3d798dffbe1ee5f545079c934a
Katharina Peter <gaz@gmx.net>
2017-07-09 17:57:32 -0700

    fixed typo in production repo config

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :wilt, Wilt.Web.Endpoint,
 
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :wilt, Wilt.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
