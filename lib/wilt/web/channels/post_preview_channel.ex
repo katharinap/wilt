c8b7acc54f4fa6dd76cab1df1df0bcb9409811ea
Katharina Peter <gaz@gmx.net>
2017-07-23 14:56:37 -0700

    made layout more responsive

--- lib/wilt/web/channels/post_preview_channel.ex
+++ lib/wilt/web/channels/post_preview_channel.ex
@@ -7,7 +7,7 @@ defmodule Wilt.Web.PostPreviewChannel do
 
   def handle_in("preview_post", %{"title" => title, "body" => body}, socket) do
     post = %Wilt.Data.Post{title: title, body: body, tags: []}
-    html = Phoenix.View.render_to_string(Wilt.Web.PostView, "_post.html", post: post)
+    html = Phoenix.View.render_to_string(Wilt.Web.PostView, "_post.html", post: post, hide: false, show_actions: false)
  
     broadcast!(socket, "live_response", %{html: html})
     {:noreply, socket}
